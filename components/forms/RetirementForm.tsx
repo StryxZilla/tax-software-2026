883a3c9f30f57d359826a1c4b44912f82f56430c
stryxzilla <stryxzilla@users.noreply.github.com>
2026-03-16 08:23:04 -0500

    Show pro-rata analysis when any Form 8606 field has value

--- components/forms/RetirementForm.tsx
+++ components/forms/RetirementForm.tsx
@@ -391,7 +391,12 @@ export default function RetirementForm({
         </div>
 
         {/* Form 8606 Analysis */}
-        {form8606 && (form8606.conversionsToRoth > 0 || form8606.nondeductibleContributions > 0) && (
+        {form8606 && (
+          (form8606.conversionsToRoth > 0 || 
+           form8606.nondeductibleContributions > 0 ||
+           form8606.priorYearBasis > 0 ||
+           form8606.distributionsFromTraditionalIRA > 0 ||
+           form8606.endOfYearTraditionalIRABalance > 0) && (
           <div className="mt-6 space-y-4 border-t pt-6">
             <h4 className="text-sm font-bold text-slate-700 uppercase tracking-wide">Pro-Rata Analysis</h4>
 
